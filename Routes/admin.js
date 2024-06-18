474d972090f03ee6b0124e913d327f2c72b7d1d4
GDhanasree <sdgidijala2005@gmail.com>
2024-06-18 23:09:55 +0530

    Initial commit

---
+++ Routes/admin.js
@@ -0,0 +1,17 @@
+const express=require("express");
+
+const router=express.Router();
+const adminUsername='admin'
+const adminPassword='admin'
+
+
+router.post("/adminLogin", (req,res)=>{
+    const {username,password}=req.body
+    if (username===adminUsername || password===adminPassword) {
+        res.send("Admin is here")
+    }
+    else{
+        res.statusCode(401).send("Unauthorized")
+    }
+})
+module.exports=router
